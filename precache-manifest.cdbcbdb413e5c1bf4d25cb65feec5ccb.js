c137602b21c9c15568817d1033740a26a53ccedb
Abdelkarim Moutik <amoutik@e1r11p3.1337.ma>
2019-06-25 15:06:03 +0100

    Updates

---
+++ precache-manifest.cdbcbdb413e5c1bf4d25cb65feec5ccb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e499d1bb14d59bf5b22159ca16bb5b",
+    "url": "/react-waste/index.html"
+  },
+  {
+    "revision": "8fbde3bbcfa4143579bb",
+    "url": "/react-waste/static/css/2.f74a8007.chunk.css"
+  },
+  {
+    "revision": "2bd5e48c02089c0ede97",
+    "url": "/react-waste/static/css/main.c1f58802.chunk.css"
+  },
+  {
+    "revision": "8fbde3bbcfa4143579bb",
+    "url": "/react-waste/static/js/2.b43bef0c.chunk.js"
+  },
+  {
+    "revision": "2bd5e48c02089c0ede97",
+    "url": "/react-waste/static/js/main.81c79f9e.chunk.js"
+  },
+  {
+    "revision": "82a5ff3e348813025e2a",
+    "url": "/react-waste/static/js/runtime~main.ffe5c8e2.js"
+  },
+  {
+    "revision": "68265982d229cb760ffbf25d2e569507",
+    "url": "/react-waste/static/media/home.68265982.jpg"
+  }
+]);
